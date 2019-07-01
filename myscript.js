a7718f11d5c05c300bf0486b6cc985cd553b6af7
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-01 17:57:28 +0200

    train ticket calculator

--- myscript.js
+++ myscript.js
@@ -9,6 +9,26 @@ uno sconto del 20% per i minorenni e
 del 40% per gli over 65 */
 
 // Dichiarazione variabili
+var km, age, costkm, cost;
 
+// Invocazione variabili
+ // Chiediamo all'utente il numero di chilometri che deve fare
+km = prompt("Inserisci la distanza in km che devi percorrere");
 
-// Assegnazione variabili
+ // Chiediamo all'utente la sua età
+age = prompt("Inserisci la tua età");
+
+ // Calcoliamo il costo rapporato ai chilometri
+costkm = km * 0.21;
+
+ // Calcoliamo il costo rapporato all'età
+if (age <= 17) {
+  cost = (costkm - ((costkm * 20) / 100)).toFixed(2);
+} else if (age >= 66) {
+  cost = (costkm - ((costkm * 40) / 100)).toFixed(2);
+} else {
+  cost = costkm;
+}
+
+// Comunichiamo all'utente il costo del biglietto
+document.getElementById('traincost').innerHTML = "Il costo del tuo biglietto è " + cost + " euro";
